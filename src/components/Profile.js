8c99a4ac6ea613362b6c681df21d0b89ac1518bf
ShubhShukla369 <shuklashubh818@gmail.com>
2023-10-20 16:36:05 +0530

    pull changes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -63,35 +63,46 @@ const Profile= () => {
                                                                 <div className="m-b-25">
                                                                     <img src="https://img.icons8.com/bubbles/100/000000/user.png" className="img-radius" alt="User-Profile-Image"/>
                                                                 </div>
-                                                                <h6 className="f-w-600">Hembo Tingor</h6>
-                                                                <p>Web Designer</p>
+                                                                <h6 className="f-w-600">{studentName}</h6>
+                                                                <p>{city}</p>
                                                                 <i className=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
                                                             </div>
                                                         </div>
                                                         <div className="col-sm-8">
                                                             <div className="card-block">
-                                                                <h6 className="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
+                                                                <h6 className="m-b-20 p-b-5 b-b-default f-w-600">Student Panel</h6>
                                                                 <div className="row">
                                                                     <div className="col-sm-6">
                                                                         <p className="m-b-10 f-w-600">Email</p>
-                                                                        <h6 className="text-muted f-w-400">rntng@gmail.com</h6>
+                                                                        <h6 className="text-muted f-w-400">{email}</h6>
                                                                     </div>
                                                                     <div className="col-sm-6">
-                                                                        <p className="m-b-10 f-w-600">Phone</p>
-                                                                        <h6 className="text-muted f-w-400">98979989898</h6>
+                                                                        <p className="m-b-10 f-w-600">Phone Number</p>
+                                                                        <h6 className="text-muted f-w-400">{phoneNumber}</h6>
                                                                     </div>
                                                                 </div>
-                                                                <h6 className="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Projects</h6>
+                                                                <h6 className="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">{course}</h6>
                                                                 <div className="row">
                                                                     <div className="col-sm-6">
-                                                                        <p className="m-b-10 f-w-600">Recent</p>
-                                                                        <h6 className="text-muted f-w-400">Sam Disuja</h6>
+                                                                        <p className="m-b-10 f-w-600">Age</p>
+                                                                        <h6 className="text-muted f-w-400">{age}</h6>
                                                                     </div>
                                                                     <div className="col-sm-6">
-                                                                        <p className="m-b-10 f-w-600">Most Viewed</p>
-                                                                        <h6 className="text-muted f-w-400">Dinoter husainm</h6>
+                                                                        <p className="m-b-10 f-w-600">Address</p>
+                                                                        <h6 className="text-muted f-w-400">{address}</h6>
                                                                     </div>
                                                                 </div>
+                                                                <div className="row ">
+                                                                    <div className="col-sm-6 ">
+                                                                        <p className="m-b-10 f-w-600">PinCode</p>
+                                                                        <h6 className="text-muted f-w-400">{pinCode}</h6>
+                                                                    </div>
+                                                                    <div className="col-sm-6">
+                                                                        <p className="m-b-10 f-w-600">State</p>
+                                                                        <h6 className="text-muted f-w-400">{state}</h6>
+                                                                    </div>
+                                                                </div>
+                                                                
                                                                 <ul className="social-link list-unstyled m-t-40 m-b-10">
                                                                     <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i className="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
                                                                     <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i className="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
